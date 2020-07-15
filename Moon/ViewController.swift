c8e7795137fd2cc13510d5c4603dd3672c7aad6a
Ramzi Chamcham <ramzichamcham@gmail.com>
2020-07-14 21:32:48 -0400

    Initial commit

--- Moon/ViewController.swift
+++ Moon/ViewController.swift
@@ -23,11 +23,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let node = SCNNode()
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let sphere = SCNSphere(radius: 0.2)
+        
+        sphere.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
+        
+        node.geometry = sphere
+        
+        node.position = SCNVector3(0, 0.1, -0.5)
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
+        
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -49,27 +59,4 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
